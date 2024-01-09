4d938d2491ed628080de1d90a479c5f31bf86eb9
Jeremiah Fallin <jeremiah.fallin@gmail.com>
2024-01-08 23:25:03 -0800

    update SingleList.jsx according to feedback.

--- src/components/SingleList.jsx
+++ src/components/SingleList.jsx
@@ -1,5 +1,6 @@
 import './SingleList.css';
 
+// Takes the name of the shopping list, the path in Firebase to the shopping list, and the set function as variables.
 export function SingleList({ name, path, setListPath }) {
 	function handleClick() {
 		setListPath(path);
